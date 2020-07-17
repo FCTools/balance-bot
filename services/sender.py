30afc7116c19740d437e3a31fea040740e6855a9
German Yakimov <german13yakimov@gmail.com>
2020-07-17 14:13:59 +0700

    Add keyboard to Sender.

--- services/sender.py
+++ services/sender.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import requests
@@ -7,6 +8,18 @@ from services import requests_manager
 class Sender:
     def __init__(self, telegram_access_token):
         self._requests_url = f"https://api.telegram.org/bot{telegram_access_token}/"
+        self._basic_keyboard = json.dumps(
+            {
+                "keyboard": [
+                    [{"text": "/get_balance"}],
+                    [{"text": "/get_balance prop"}, {"text": "/get_balance eva"}],
+                    [{"text": "/get_balance pushhouse"}, {"text": "/get_balance dao"}],
+                    [{"text": "/help"}],
+                ],
+                "resize_keyboard": True,
+            }
+        )
+
         self._logger = logging.getLogger("WorkingLoop.Sender")
 
         self._logger.info("Sender initialized.")
@@ -15,7 +28,9 @@ class Sender:
         method = "sendMessage"
 
         response = requests_manager.post(
-            requests.Session(), self._requests_url + method, params={"chat_id": to, "text": text, "parse_mode": "HTML"}
+            requests.Session(),
+            self._requests_url + method,
+            params={"chat_id": to, "text": text, "parse_mode": "HTML", "reply_markup": self._basic_keyboard},
         )
 
         if not isinstance(response, requests.Response):
