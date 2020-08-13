d5f2264432900bca93201e78d903897da1478af0
German Yakimov <german13yakimov@gmail.com>
2020-08-13 15:44:28 +0600

    Add docstrings to requests_manager.py

--- services/requests_manager.py
+++ services/requests_manager.py
@@ -7,6 +7,10 @@ import requests
 
 
 def catch_network_errors(method):
+    """
+    Decorator for network erros catching.
+    """
+
     def wrapper(*args, **kwargs):
         try:
             return method(*args, **kwargs)
@@ -25,9 +29,29 @@ def catch_network_errors(method):
 
 @catch_network_errors
 def get(session, *args, **kwargs):
+    """
+    Make safe GET response using given session and arguments.
+
+    :param session: session for response making
+    :type session: requests.Session
+
+    :return: response if success, else catched error
+    :rtype: Union[requests.Response, Exception]
+    """
+
     return session.get(*args, **kwargs)
 
 
 @catch_network_errors
 def post(session, *args, **kwargs):
+    """
+    Make safe POST response using given session and arguments.
+
+    :param session: session for response making
+    :type session: requests.Session
+
+    :return: response if success, else catched error
+    :rtype: Union[requests.Response, Exception]
+    """
+
     return session.post(*args, **kwargs)
