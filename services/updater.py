8613e730f444b654632a1b6e8c6d65dd2c8659f4
German Yakimov <german13yakimov@gmail.com>
2020-08-13 17:26:41 +0600

    Add docstrings to Updater.

--- services/updater.py
+++ services/updater.py
@@ -7,10 +7,27 @@ import requests
 
 
 class Updater:
+    """
+    Service for telegram updates getting.
+    """
+
     def __init__(self, telegram_access_token):
         self._requests_url = f"https://api.telegram.org/bot{telegram_access_token}/"
 
     def get_updates(self, offset=None, timeout=10):
+        """
+        Get updates list from telegram.
+
+        :param offset: offset (last update id)
+        :type offset: int
+
+        :param timeout: timeout
+        :type timeout: int
+
+        :return: updates list
+        :rtype: list
+        """
+
         method = "getUpdates"
 
         updates = requests.get(self._requests_url + method, params={"offset": offset, "timeout": timeout}).json()
